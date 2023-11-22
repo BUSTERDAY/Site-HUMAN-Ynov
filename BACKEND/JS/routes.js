6186be1c6f4d52353da58d58e7ea7fc4c5d20fe2
MONFORTE Pierre <95189614+BUSTERDAY@users.noreply.github.com>
2023-11-22 08:56:05 +0100

    modif pour convenir au site human ynov

--- BACKEND/JS/routes.js
+++ BACKEND/JS/routes.js
@@ -5,27 +5,17 @@ const port = 8080;
 
 
 
-const stage = JSON.stringify([
-    { entreprise: "", description:"cuisine de restaurant, plonge", mois : "", annee: 0 },
-    { entreprise: "Marine Nationale", description:"découverte du PEM à la Seyne sur Mer", mois : "", annee: 0 }
-]);
-
-const emploi = JSON.stringify([
-    { entreprise: "Panier Provencal", description:"emploi de saisonnier, trie de tomate", mois : "juillet-août", annee: 2021 },
-    { entreprise: "", description:"", mois : "", annee: 0 }
+const evenement = JSON.stringify([
+    { event: "Don du Sang", description:"don du sang en partenariat avec la branche Aixoise", mois : "", annee: 0 }
 ]);
 
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
     switch (req.url) {
-        case "/stage":
+        case "/evenement":
             res.writeHead(200);
-            res.end(stage);
-            break
-        case "/emploi":
-            res.writeHead(200);
-            res.end(emploi);
-            break
+            res.end(evenement);
+            break;
         default:
             res.writeHead(404);
             res.end(JSON.stringify({error:"Resource not found"}));
